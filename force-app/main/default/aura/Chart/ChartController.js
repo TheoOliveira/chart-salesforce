655be460e574770af7aadeaac1f76eaf749c9f2e
TheoOliveira <akai.hen@gmail.com>
2020-06-18 16:16:20 -0300

    termino

--- force-app/main/default/aura/Chart/ChartController.js
+++ force-app/main/default/aura/Chart/ChartController.js
@@ -1,17 +1,46 @@
-({	// controller js padrão puxa o metodo do apex 
+({	
 	ctr : function(cmp, event, helper) {
         var temp = [];
+        var temp1 = [];
+        var temp2 = [];
+        var temp3 = [];
+
         var action = cmp.get("c.getChartMap");
         action.setCallback(this, function(response){
             if(response.getState() === 'SUCCESS' && response.getReturnValue()){
                 temp = response.getReturnValue();
-                console.log(response.getState());
-                console.log(temp);
-                helper.createGraph(cmp, temp);
+                helper.createGraphAumentoLimite(cmp, temp);
             }
-        });      
-          
-       $A.enqueueAction(action);	
+        });
+        var action1 = cmp.get("c.getChartMap");
+        action1.setCallback(this, function(response){
+            if(response.getState() === 'SUCCESS' && response.getReturnValue()){
+                temp1 = response.getReturnValue();
+                helper.createGraphAlteracaoExposicao(cmp, temp1);
+            }
+        });
+        var action2 = cmp.get("c.getChartMap");
+        action2.setCallback(this, function(response){
+            if(response.getState() === 'SUCCESS' && response.getReturnValue()){
+                temp2 = response.getReturnValue();
+                helper.createGraphAlteracaoFaturamento(cmp, temp2);
+            }
+        });
+        
+        var action3 = cmp.get("c.getChartMap");
+        action3.setCallback(this, function(response){
+            if(response.getState() === 'SUCCESS' && response.getReturnValue()){
+                temp3 = response.getReturnValue();
+                helper.createGraphTaxasTarifas(cmp, temp3);
+            }
+        });
+         
+       $A.enqueueAction(action);
+       $A.enqueueAction(action1);
+       $A.enqueueAction(action2);	
+       $A.enqueueAction(action3);	
+
+
        
 	}
 })
--- force-app/main/default/aura/Chart/ChartHelper.js
+++ force-app/main/default/aura/Chart/ChartHelper.js
@@ -1,65 +1,430 @@
-({ // puxa o map do apex e joga o key para label e o outro para data
-    createGraph : function(cmp, temp) {
-        
-        var dataMap = {"chartLabels": Object.keys(temp),
-                       "chartData": Object.values(temp)
-                       };
-        
-        var el = cmp.find('barChart').getElement();
-        var ctx = el.getContext('2d');
-        
-        if(el.id == 'barChart'){
-            console.log('não invertido');
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: dataMap.chartLabels,
-                    datasets: [
-                        {
-                            label: "Payments History",
-                            backgroundColor: "rgba(153,255,51,0.5)",
-                            data: dataMap.chartData
-                        }
-                    ]
-                },             
+({
+    createGraphAumentoLimite: function(cmp, temp) {
+      var dataMap = {
+        chartLabels: Object.keys(temp),
+        chartData: Object.values(temp)
+      };
+      var scopeOptions = {
+        categoryPercentage: 1.0,
+        barPercentage: 1.0,
+        responsive: false,
+  
+        legend: {
+          display: true,
+          position: "bottom"
+        },
+        tooltips: {
+          enabled: false
+        },
+  
+        scales: {
+          xAxes: [
+            {
+              display: true, //this will remove all the x-axis grid lines
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontColor: "#fff",
+                fontStyle: "normal",
+                fontSize: 13,
+  
+                beginAtZero: true
+              }
+            }
+          ],
+          yAxes: [
+            {
+              display: false,
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        },
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function() {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+  
+            ctx.font = Chart.helpers.fontString(
+              Chart.defaults.global.defaultFontSize,
+              Chart.defaults.global.defaultFontStyle,
+              Chart.defaults.global.defaultFontFamily
+            );
+            ctx.textAlign = "center";
+            ctx.textBaseline = "bottom";
+  
+            this.data.datasets.forEach(function(dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index];
+                function retornaReal(currencyData) {
+                  var data =
+                    "R$" +
+                    String(currencyData).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                  return data;
+                }
+  
+                ctx.fillText(retornaReal(data), bar._model.x, bar._model.y - 5);
+              });
             });
-        }if(el.id == 'barChartInverted'){
-            console.log('invertido');
-            new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: dataMap.chartLabels,
-                    datasets: [
-                        {
-                            label: "Payments History",
-                            backgroundColor: "rgba(153,255,51,0.5)",
-                            data: dataMap.chartData
-                        }
-                    ]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                    reverse: true,
-                                beginAtZero:true
-                            }
-                        }],
-                        xAxes: [{
-                                ticks: {
-                                reverse: true,
-                              beginAtZero: true
-                            }
-                        }]
-                    }
-                }             
-            });
+          }
         }
-        
-        
+      };
+      //GRAFICO PARA ALTERAÇÃO DE LIMITE
+      var el = cmp.find("aumentoChart").getElement();
+      var ctx = el.getContext("2d");
+  
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          datasets: [
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(245, 171, 53, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(245, 171, 53, 1)",
+              data: dataMap.chartData
+            }
+          ]
+        },
+        options: scopeOptions
+      });
     },
-	
-
-    
-    
-})
+    createGraphAlteracaoExposicao: function(cmp, temp1) {
+      var dataMap = {
+        chartLabels: Object.keys(temp1),
+        chartData: Object.values(temp1)
+      };
+      //GRAFICO PARA ALTERAÇÃO EXPOSIÇÃO
+      var scopeOptions = {
+        categoryPercentage: 1.0,
+        barPercentage: 1.0,
+        responsive: false,
+  
+        legend: {
+          display: true,
+          position: "bottom"
+        },
+        tooltips: {
+          enabled: false
+        },
+  
+        scales: {
+          xAxes: [
+            {
+              display: true, //this will remove all the x-axis grid lines
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontColor: "#fff",
+                fontStyle: "normal",
+                fontSize: 13,
+  
+                beginAtZero: true
+              }
+            }
+          ],
+          yAxes: [
+            {
+              display: false,
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        },
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function() {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+  
+            ctx.font = Chart.helpers.fontString(
+              Chart.defaults.global.defaultFontSize,
+              Chart.defaults.global.defaultFontStyle,
+              Chart.defaults.global.defaultFontFamily
+            );
+            ctx.textAlign = "center";
+            ctx.textBaseline = "bottom";
+  
+            this.data.datasets.forEach(function(dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index];
+                function retornaReal(currencyData) {
+                  var data =
+                    "R$" +
+                    String(currencyData).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                  return data;
+                }
+                ctx.fillText(retornaReal(data), bar._model.x, bar._model.y - 5);
+              });
+            });
+          }
+        }
+      };
+  
+      var el = cmp.find("exposicaoChart").getElement();
+      var ctx = el.getContext("2d");
+  
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          datasets: [
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(245, 171, 53, 1)",
+              data: dataMap.chartData
+            }
+          ]
+        },
+        options: scopeOptions
+      });
+    },
+    createGraphAlteracaoFaturamento: function(cmp, temp2) {
+      var dataMap = {
+        chartLabels: Object.keys(temp2),
+        chartData: Object.values(temp2)
+      };
+      // GRÁFICO PARA ALTERAÇÃO FATURAMENTO
+      var scopeOptions = {
+        categoryPercentage: 1.0,
+        barPercentage: 1.0,
+        responsive: false,
+  
+        legend: {
+          display: true,
+          position: "bottom"
+        },
+        tooltips: {
+          enabled: false
+        },
+  
+        scales: {
+          xAxes: [
+            {
+              display: true, //this will remove all the x-axis grid lines
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontColor: "#fff",
+                fontStyle: "normal",
+                fontSize: 17,
+  
+                beginAtZero: true,
+                reverse: false
+              }
+            }
+          ],
+          yAxes: [
+            {
+              display: false,
+              ticks: {
+                beginAtZero: true,
+                reverse: true
+              }
+            }
+          ]
+        },
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function() {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+  
+            ctx.font = Chart.helpers.fontString(
+              Chart.defaults.global.defaultFontSize,
+              Chart.defaults.global.defaultFontStyle,
+              Chart.defaults.global.defaultFontFamily
+            );
+            ctx.textAlign = "center";
+            ctx.textBaseline = "bottom";
+  
+            this.data.datasets.forEach(function(dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index];
+                function retornaReal(currencyData) {
+                  var data =
+                    "R$" +
+                    String(currencyData).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                  return data;
+                }
+                ctx.fillText(retornaReal(data), bar._model.x, bar._model.y + 15);
+              });
+            });
+          }
+        }
+      };
+  
+      var el = cmp.find("faturamentoChart").getElement();
+      var ctx = el.getContext("2d");
+  
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          datasets: [
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(245, 171, 53, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            }
+          ]
+        },
+        options: scopeOptions
+      });
+    },
+    createGraphTaxasTarifas: function(cmp, temp3) {
+      var dataMap = {
+        chartLabels: Object.keys(temp3),
+        chartData: Object.values(temp3)
+      };
+      // GRÁFICO ALTERAÇÃO TAXAS E TARIFAS
+      var scopeOptions = {
+        categoryPercentage: 0.7,
+        barPercentage: 1.0,
+        responsive: false,
+  
+        legend: {
+          display: true,
+          position: "bottom"
+        },
+        tooltips: {
+          enabled: false
+        },
+  
+        scales: {
+          xAxes: [
+            {
+              display: true, //this will remove all the x-axis grid lines
+              gridLines: {
+                display: false
+              },
+              ticks: {
+                fontColor: "#fff",
+                fontStyle: "normal",
+                fontSize: 13,
+  
+                beginAtZero: true
+              }
+            }
+          ],
+          yAxes: [
+            {
+              display: false,
+              ticks: {
+                beginAtZero: true
+              }
+            }
+          ]
+        },
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function() {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+  
+            ctx.font = Chart.helpers.fontString(
+              Chart.defaults.global.defaultFontSize,
+              Chart.defaults.global.defaultFontStyle,
+              Chart.defaults.global.defaultFontFamily
+            );
+            ctx.textAlign = "center";
+            ctx.textBaseline = "bottom";
+  
+            this.data.datasets.forEach(function(dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function(bar, index) {
+                var data = dataset.data[index];
+                function retornaReal(currencyData) {
+                  var data =
+                    "R$" +
+                    String(currencyData).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                  return data;
+                }
+                ctx.fillText(retornaReal(data), bar._model.x, bar._model.y - 5);
+              });
+            });
+          }
+        }
+      };
+  
+      var el = cmp.find("taxaChart").getElement();
+      var ctx = el.getContext("2d");
+  
+      new Chart(ctx, {
+        type: "bar",
+        data: {
+          datasets: [
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(245, 171, 53, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              data: dataMap.chartData
+            },
+            {
+              label: dataMap.chartLabels,
+              backgroundColor: "rgba(245, 171, 53, 1)",
+              data: dataMap.chartData
+            }
+          ]
+        },
+        options: scopeOptions
+      });
+    }
+  });
+  
